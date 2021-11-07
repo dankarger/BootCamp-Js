d8ac58163016645225c69804e3886e8706730435
dankarger <karger.dan@gmail.com>
2021-11-07 15:36:01 +0200

    exrecise 2.4

---
+++ js/2.4.js
@@ -0,0 +1,10 @@
+function whatsTheDate() {
+    let currentDate = new Date();
+    let day  = currentDate.toLocaleString('en-us', {weekday: 'long'});
+    let dayOfMonth = currentDate.getDate();
+    let month = currentDate.toLocaleString('default', { month: 'long' });
+    let year = currentDate.getFullYear();
+    return `Today is ${day} the ${dayOfMonth} of ${month},${year}`
+}
+
+console.log(whatsTheDate())
