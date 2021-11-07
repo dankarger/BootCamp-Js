3b57eec50b72da685bf5f6ca1061f8d23fed9acb
dankarger <karger.dan@gmail.com>
2021-11-07 11:30:44 +0200

    exrecise 1.1

---
+++ js/1.1.js
@@ -0,0 +1,15 @@
+let strVar = "Hi I'm a string";
+let numVar = 19;
+let objVar = {'first':'12'};
+let booleanVar = true;
+let nullVar= null;
+let undefinedVar = undefined;
+let symbolVar = Symbol('#')
+
+console.log(strVar);
+console.log(numVar);
+console.log(objVar);
+console.log(booleanVar);
+console.log(nullVar);
+console.log(undefinedVar);
+console.log(symbolVar);
