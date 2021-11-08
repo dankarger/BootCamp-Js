f6258aa88bc0e69f86492b85f25e8a226a95eb2d
dankarger <karger.dan@gmail.com>
2021-11-08 13:16:49 +0200

    exrecise 4.1

---
+++ js/4.1.js
@@ -0,0 +1,8 @@
+let numSibling = Number(prompt('How many sibling do you have?'));
+if(numSibling===1){
+    console.log('1Sibling');
+}else if(numSibling>1){
+    console.log('more than 1 sibling');
+}else{
+    console.log('No sibling')
+}
