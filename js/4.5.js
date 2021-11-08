492ad4ee78d84f2b2b712983107eb2bf802d4f2a
dankarger <karger.dan@gmail.com>
2021-11-08 14:53:31 +0200

    exrecise 4.5

---
+++ js/4.5.js
@@ -0,0 +1,17 @@
+function leapYear(year) {
+    if(year%4===0){
+         if(year%100===0&&year%400!==0){
+             return 'This is not a leap year'
+             }else {
+             return 'This is indeed a leap year **'}
+    }else {
+        return 'This is not a leap year'
+    }
+}
+
+console.log(leapYear(2024))
+//test
+
+for(let i=2000;i<=2050;i++) {
+    console.log(`${i}- ${leapYear(i)}`)
+}
