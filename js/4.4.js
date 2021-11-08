9370a410573c391ce5319daffcfacbc69d1f8853
dankarger <karger.dan@gmail.com>
2021-11-08 14:36:41 +0200

    exrecise 4.4

--- js/4.4.js
+++ js/4.4.js
@@ -2,7 +2,7 @@ function boom(n){
     let result =[];
     for(let i=1;i<=n;i++){
         if(i%7===0&&i.toString().includes('7')){
-            result .push('Boom-Boom');
+            result.push('Boom-Boom');
         }
         else if (i%7===0){
             result.push('Boom');
