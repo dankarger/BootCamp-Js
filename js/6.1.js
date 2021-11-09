1337bb7ce49c082207dae112b3bb834d5fea8882
dankarger <karger.dan@gmail.com>
2021-11-09 13:33:31 +0200

    exrecise 6.1

---
+++ js/6.1.js
@@ -0,0 +1,35 @@
+const arr=[1,7,3,-5,7,3,9];
+
+//
+for(let n in arr){
+    console.log(arr[n])
+}
+
+function arrayLength(arr) {
+    let arrLength = 0;
+    for(let n in arr) {
+        arrLength +=1;
+    }
+    return arrLength
+}
+
+console.log(arrayLength(arr))
+
+function arraySum(arr) {
+    let sum=0;
+    for(let i=0; i<arr.length;i++) {
+        sum+=arr[i];
+    }
+    return sum;
+}
+console.log(arraySum(arr))
+
+function arrayMulti(arr) {
+    let multi=1;
+    for(let n in arr) {
+        multi *= arr[n];
+    }
+    return multi;
+}
+
+console.log(arrayMulti(arr))
