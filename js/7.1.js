aab4460e104a24d45f2386706001f4545def7d73
dankarger <karger.dan@gmail.com>
2021-11-10 11:11:14 +0200

    exrecise 7.1

---
+++ js/7.1.js
@@ -0,0 +1,9 @@
+const arr = [3,4,4,3,6,3,3,4,5,6,9,0,0,0]
+
+function removerDuplicate(arr) {
+    let result =[];
+    arr.forEach(element=> {if(result.indexOf(element)===-1){result.push(element)}})
+    return result;
+}
+
+console.log(removerDuplicate(arr))
