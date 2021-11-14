ec6174691b03b594f71b91b1c0f738198aea334f
dankarger <karger.dan@gmail.com>
2021-11-14 12:33:20 +0200

    exrecise 9.4

---
+++ js/9.4.js
@@ -0,0 +1,19 @@
+function steps(n) {
+    let result = [];
+    for (let i = 0; i <= n; i++) {
+        // result[i]= ' ';
+        result[i]=''
+        for (let j = n; j > i; j--) {
+            result[i]+=('#');
+        }
+        for(let t=n-result[i].length;t>0;t--) {
+            result[i] += ' ';
+        }
+        console.log(result[i])
+    }
+    result.pop()
+    // return result.reverse();
+    return result.reverse().join('\n' )
+}
+
+console.log(steps(4))
