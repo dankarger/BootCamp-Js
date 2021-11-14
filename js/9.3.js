5aff0445d85e27f7044d6b44fc0621ce47c89d10
dankarger <karger.dan@gmail.com>
2021-11-14 11:20:52 +0200

    exrecise 9.3

---
+++ js/9.3.js
@@ -0,0 +1,16 @@
+const food = ["Noodle", "Pasta", "Ice-cream", "Meat", "Cucumber", "Olives"];
+const food1 = ["Fries", "Ice-cream", "Pizza", "Olives", "Hamburgers"];
+
+function compareTwoStrings(a, b) {
+    let result = [];
+    for (let i = 0; i < a.length; i++) {
+        for (let j = 0; j < b.length; j++) {
+            if (a[i] === b[j]) {
+                result.push(a[i])
+            }
+        }
+    }
+    return result;
+}
+
+console.log(compareTwoStrings(food, food1))
