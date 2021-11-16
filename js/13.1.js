fdebce890b7ab01e3798c3d5808faf8d49165689
dankarger <karger.dan@gmail.com>
2021-11-16 12:45:34 +0200

    exrecise 13.1

---
+++ js/13.1.js
@@ -0,0 +1,9 @@
+const numbers = [1, -5, 666, 2, 400, 11];
+
+//1
+const numbersSort1 = numbers.sort((a,b)=>a-b);
+console.log(numbersSort1)
+
+//2
+const numbersSort2 = numbers.sort((a,b)=>b-a)
+console.log(numbersSort2)
