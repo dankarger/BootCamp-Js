9e2f81a508c209ac52d66b1b8a9e9fbb5d34a958
dankarger <karger.dan@gmail.com>
2021-11-17 11:38:36 +0200

    exrecise 15.2

---
+++ js/15.2.js
@@ -0,0 +1,18 @@
+function findSmallest(a, b, c){ if (a > b > c){
+    return c;
+} else if (a > c > b) {
+    return a; } else {
+    return b; }
+}
+console.log(findSmallest(52,66, 2));
+
+
+//1
+// Problem in line 7,
+
+//2
+//I use the debugger in the webstrom.
+
+//3
+// typo in the name of the function.
+
